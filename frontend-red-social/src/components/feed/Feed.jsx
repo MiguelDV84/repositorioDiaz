23cca54f20b72590c2e976bb6dab6b90a6c4b3ab
MiguelDV84 <miguel.diazv84@gmail.com>
2023-05-22 21:01:00 +0200

    Styles feed
    Added icon options publication

--- frontend-red-social/src/components/feed/Feed.jsx
+++ frontend-red-social/src/components/feed/Feed.jsx
@@ -4,6 +4,7 @@ import ImagePubli from "../utils/Image/ImagePubli";
 import ImageUser from "../utils/Image/ImageUser";
 import FavIcon from "@mui/icons-material/FavoriteBorder";
 import CommentIcon from "@mui/icons-material/MapsUgcRounded";
+import ListIcon from "@mui/icons-material/List";
 import "./Feed.css";
 
 export default function Feed() {
@@ -44,19 +45,22 @@ export default function Feed() {
                 <ImageUser imageName={item.user.image} />
                 <h4> {item.user.nick}</h4>
               </div>
-              <section>
-                <div className="feed__container--image">
-                  <ImagePubli imageName={item.file} />
-                </div>
-                <div className="feed__container--icons">
-                  <FavIcon /> <CommentIcon />
-                </div>
-                <p>{item.text}</p>
-              </section>
-              <footer>
-                <p>{item.created_at}</p>
-              </footer>
+              <ListIcon />
             </header>
+            <section>
+              <div className="feed__container--image">
+                <ImagePubli imageName={item.file} />
+              </div>
+              <div className="feed__container--icons">
+                <FavIcon /> <CommentIcon />
+              </div>
+              <p>
+                <strong>{item.user.nick}</strong> {item.text}
+              </p>
+            </section>
+            <footer>
+              <p>{item.created_at}</p>
+            </footer>
           </article>
         ))
       ) : (
